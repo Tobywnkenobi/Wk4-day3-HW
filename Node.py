29f03de8067c5a7cd5c9f637ed85c1f83c274d7f
Tobywnkenobi <141949401+Tobywnkenobi@users.noreply.github.com>
2023-09-14 00:24:57 -0400

    the Readme is in the sandbox

--- Node.py
+++ Node.py
@@ -1,12 +1,85 @@
-class Node:
+class node:
+    def __init__(self, data=None):
+        self.data=data
+        self.next=None
 
-  def __init__(self, value):
-    self.value = value
-    self.right = None
-    self.left = None
+class linked_list:
+    def __init__(self):
+        self.head = node()
 
-  def __repr__(self):
-    return f'<Node: {self.value}>'
+    def append(self,data):
+        new_node = node(data)
+        cur = self.head
+        while cur.next != None:
+            cur = cur.next
+        cur.next = new_node
+
+    def length(self):
+        cur = self.head
+        total = 0
+        while cur.next!=None:
+          total+=1
+          cur = cur.next
+        return total
+    
+    def display(self):
+        elems = []
+        cur_node = self.head
+        while cur_node.next != None:
+            cur_node = cur_node.next
+            elems.append(cur_node.data)
+        print(elems)
+
+    def get(self,index):
+        if index>=self.length():
+            print ("Error, index out of range")
+            return None
+        cur_idx=0
+        cur_node=self.head
+        while True:
+            cur_node=cur.node.next
+            if cur_idx==index:return cur_node.data
+            cur_idx==1
+
+    def erase(self,index):
+        if index == self.len():
+            print("Error, erase")
+            return
+        cur_idx=0
+        cur_node = self.head
+        while True:
+            last_node = cur_node
+            cur_node = cur_node.next
+            if cur_idx==index:
+                last_node.next = cur_node.next
+                return
+            cur_idx+=1
+
+
+
+my_list = linked_list()
+
+my_list.display()
+
+my_list.append(1)
+my_list.append(23)
+my_list.append(3)
+my_list.append(4)
+
+my_list.display()
+
+
+
+
+# class Node:
+
+#   def __init__(self, value):
+#     self.value = value
+#     self.right = None
+#     self.left = None
+
+#   def __repr__(self):
+#     return f'<Node: {self.value}>'
 
 # node = Node('Monday')
 
---
+++ sandbox.py
@@ -0,0 +1,8 @@
+""""
+Create Linked List 
+
+
+I did try to go back through a couple of tutorials about doing this, and coded along with them.  I understand the concept of the single and double linked lists overall.  in the single list, you start at the beginning, and you check each node until you find the one that you are looking for.  It is not as effcient as the double length lists, although they require the most memory.  The concept of the double is that it splits in two directions from the head, and you can still traverse each node across them.  The key is to close the hole.  you can easily remove a node by skipping it, essentially deleting it.  You can also easily add nodes.  The problem for me is seeing where all of this play into the code.  I understand that this allows us to move through super large data sets, and some of the implicatons, but more of just seeing how it all functions on a deeper level.
+
+
+"""
